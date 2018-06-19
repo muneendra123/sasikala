4c0579346c933499be3927049abad8961dd54936
muneendra <muneendra650@gmail.com>
2018-06-19 16:06:22 +0530

    java

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,14 +2,18 @@
  * @author Petri Kainulainen
  */
 public class FizzBuzz {
+
     public String getFizzBuzzWord(int number) {
         StringBuilder fizzBuzzWord = new StringBuilder();
+
         if (number % 3 == 0) {
             fizzBuzzWord.append("Fizz");
-			}
+        }
+
         if (number % 5 == 0) {
             fizzBuzzWord.append("Buzz");
         }
+
         return (fizzBuzzWord.length() == 0) ? null : fizzBuzzWord.toString();
     }
 }
